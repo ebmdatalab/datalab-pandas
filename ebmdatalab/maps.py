1d286bb9e28a05e49a12d0176695c9606163f018
CarolineMorton <carolinemorton@gmx.com>
2019-09-17 09:25:18 +0100

    update to module with class for maps

--- ebmdatalab/maps.py
+++ ebmdatalab/maps.py
@@ -1,131 +1,124 @@
-import glob
 import geopandas as gpd
-import numpy as np
 import pandas as pd
-import matplotlib.pyplot as plt
 import matplotlib.gridspec as gridspec
-from pathlib import Path
+import matplotlib.pyplot as plt
+from mpl_toolkits.axes_grid1 import make_axes_locatable
+import datetime
 
+plt.ioff()
 
-def ccg_map(
-    df,
-    title="",
-    column=None,
-    separate_london=False,
-    london_layout="horizontal",
-    cartogram=False,
-    subplot_spec=None,
-    show_legend=True,
-    map_year=None,
-    plot_options=None,
-):
-    """Draw a CCG map with London separated out
+class MapLoader():
+    """ Map Loader for loading in python df and creating maps
+
     """
-    # Because this uses subplots to arrange London and England,
-    # the only way to arrange nested subplots is via a subplotspec
-    assert column, "You must specify a column name to plot"
-    df = df.copy()
-    # input df must have 'pct' column, plus others as specified
-    data_dir = Path(__file__).parent / "data"
 
-    # Add names and if it's London. Note the names in ccg_for_map must
-    # match names in the CCG geojson, as that doesn't include codes at
-    # the momemt
-    names = pd.read_csv(data_dir / "ccg_for_map.csv")
+    def __init__(self, description=None, author="EBM DataLab", *args, **kwargs):
+        """Map Loader is initiated with description and author for clarity
 
-    # Check we know about all the codes in the input data
-    diff = np.setdiff1d(df["pct"], names["code"])
-    if len(diff) > 0:
-        raise BaseException(
-            "Data contains CCG codes we don't know about: {}".format(diff)
-        )
+            Args:
+                description (str): Description at a broad level of the dataset
+                author (str): Author - set as EBM Datalab if left blank
 
-    df = df.merge(names[["code", "name", "is_london"]], left_on="pct", right_on="code")
-    df = df.set_index("name")
+        """
 
-    # Load map data
-    cartogram_suffix = ""
-    if cartogram:
-        cartogram_suffix = "_cartogram"
-    if map_year:
-        map_file = data_dir / "ccgs{}_{}.json".format(cartogram_suffix, map_year)
-    else:
-        map_file = sorted(
-            glob.glob(str(data_dir / "ccgs{}_2*.json".format(cartogram_suffix)))
-        )[-1]
-    ccgs = gpd.read_file(map_file)
-    # Normalise names to match `ccg_fo_map` format (above)
-    ccgs["name"] = ccgs["name"].str.upper()
-    ccgs = ccgs.set_index("name")
-    # Remove ones without geometry - these are (usually) federations
-    # rather than individual CCGs
-    ccgs = ccgs[~ccgs["geometry"].isnull()]
+        self.description = description
+        self.author = author
 
-    # Check we can map all the CCGs named in the input data
-    diff = np.setdiff1d(df.index, ccgs.index)
-    if len(diff) > 0:
-        raise BaseException("Data contains CCG names we can't map: {}".format(diff))
+        self.date = datetime.datetime.now()
+        """Datetime (obj): Automatically records datetime as time class is initiated"""
 
-    # Join map with data
-    gdf = ccgs.join(df, rsuffix="_orig")
+        self.plots = {}
+        """dict: initally blank to allow plots to be saved"""
 
-    # Split into london and rest of England
-    gdf_london = gdf[gdf["is_london"] == True]
-    gdf_roe = gdf[gdf["is_london"] == False]
+    def load_data(self, data, mapping_data_path, index_file_path=None):
+        """
+        Loads data from a pandas dataframe and combines with a mapping json file
 
-    # set common value limits for colour scale
-    default_plot_options = {
-        'vmin': df[column].min(),
-        'vmax': df[column].max(),
-        'edgecolor': "black",
-        'linewidth': 0.1,
-        'cmap': "OrRd",
-    }
+        :param data: pd.dataframe file
+        :param mapping_data_path (str): the path of the json file
+        :param index_file_path (str): path of the index file. Index file contains additionally information
+                about the geographical areas such as London False/True
 
-    if plot_options is None:
-        plot_options = {}
+        :return: self.final_df
+        """
 
-    for k, v in default_plot_options.items():
-        if k not in plot_options:
-            plot_options[k] = v
-
-    def plot(gdf, ax, title="", legend=True):
-        gdf.plot(
-            ax=ax,
-            column=column,
-            legend=legend,
-            **plot_options
-        )
-        ax.set_aspect(1.63)
-        if title:
-            ax.set_title(title, size=14)
-        ax.axis("off")
-
-    fig = plt.gcf()
-    if not subplot_spec:
-        subplot_spec = gridspec.GridSpec(1, 1)[0]
-    if separate_london:
-        if london_layout == "horizontal":
-            gs = gridspec.GridSpecFromSubplotSpec(
-                nrows=1, ncols=2, width_ratios=[1, 2], subplot_spec=subplot_spec
-            )
-            ldn_ax = fig.add_subplot(gs[0, 0])
-            roe_ax = fig.add_subplot(gs[0, 1])
+        self.map_data = gpd.read_file(mapping_data_path)
+        if index_file_path == None:
+            final = gpd.GeoDataFrame(
+                pd.merge(left=data, right=self.map_data, how='inner', left_on='row_id', right_on='code'))
+            self.final_df = final
         else:
-            gs = gridspec.GridSpecFromSubplotSpec(
-                nrows=2, ncols=1, height_ratios=[2, 1], subplot_spec=subplot_spec
-            )
-            roe_ax = fig.add_subplot(gs[0, 0])
-            ldn_ax = fig.add_subplot(gs[1, 0])
+            self.index_file = pd.read_csv(index_file_path)
+            interim = gpd.GeoDataFrame(
+                pd.merge(left=data, right=self.index_file, how='inner', left_on='row_id', right_on='code',
+                         left_index=True))
+            final = gpd.GeoDataFrame(
+                pd.merge(left=interim, right=self.map_data, how='inner', left_on='row_id', right_on='code'))
+            self.final_df = final
 
-        plot(
-            gdf_roe,
-            roe_ax,
-            title="England (excluding London):\n{}".format(title),
-            legend=show_legend,
-        )
-        plot(gdf_london, ldn_ax, title="London:\n{}".format(title), legend=False)
-    else:
-        ax = plt.subplot(subplot_spec)
-        plot(gdf, ax, title=title, legend=show_legend)
-    return plt
+    def create_map(self, plot_value, title, description=None,
+                   london_separate=True,
+                   size=(12, 10),
+                   cmap="OrRd",
+                   legend=True, fit_legend=False,
+                   edgecolor="black", linewidth=0.1, save=False):
+        """
+
+        :param plot_value (str): points to which column in the dataframe for plotting
+        :param title (str): title of the plots
+        :param description (str): description of the plots
+        :param london_separate (bool): True or False
+        :param size (2 item tuple): size of the plot
+        :param cmap (str): colors - standard use is OrRd
+        :param legend (bool): True or False
+        :param fit_legend (bool): True or False. Scales the legend to the size of the map
+        :param edgecolor (str): Set to black
+        :param linewidth (float): Width of the line around geographical areas
+        :param save (bool): True or False. If True, saves in self.plots as a dictionary with description as key, plot as value
+        :return: Figure
+        """
+
+        subplot_spec = gridspec.GridSpec(1, 1)[0]
+        if london_separate == True:
+            lon_data = self.final_df[self.final_df["is_london"] == True]
+            roe_data = self.final_df[self.final_df["is_london"] == False]
+            gs = gridspec.GridSpecFromSubplotSpec(nrows=1, ncols=2, width_ratios=[1, 2], subplot_spec=subplot_spec)
+            fig = plt.figure(figsize=size)
+            ldn_ax = fig.add_subplot(gs[0, 0])
+            ldn_ax.axis("off")
+            ldn_ax.set_title("London:\n{}".format(title), size=14)
+            roe_ax = fig.add_subplot(gs[0, 1])
+            if fit_legend == True:
+                divider = make_axes_locatable(roe_ax)
+                cax = divider.append_axes("right", size="5%", pad=0.1)
+            else:
+                cax = None
+            roe_ax.axis("off")
+            roe_ax.set_title("England (excluding London):\n{}".format(title))
+            roe_data.plot(column=plot_value, legend=legend, ax=roe_ax, cmap=cmap, cax=cax, edgecolor=edgecolor,
+                          linewidth=linewidth)
+            lon_data.plot(column=plot_value, legend=False, ax=ldn_ax, cmap=cmap, edgecolor=edgecolor,
+                          linewidth=linewidth)
+            if save == True:
+                self.plots[description] = fig
+            plt.close()
+            return fig
+        elif london_separate == False:
+            dt = self.final_df
+            fig = plt.figure(figsize=size)
+            uk_ax = fig.add_subplot(111)
+            uk_ax.axis("off")
+            uk_ax.set_title("UK:\n{}".format(title), size=14)
+            if fit_legend == True:
+                divider = make_axes_locatable(uk_ax)
+                cax = divider.append_axes("right", size="5%", pad=0.1)
+            else:
+                cax = None
+            dt.plot(column=plot_value, legend=legend, ax=uk_ax, cmap=cmap, cax=cax, edgecolor=edgecolor,
+                    linewidth=linewidth)
+            if save == True:
+                self.plots[description] = fig
+            return fig
+        else:
+            pass
+
